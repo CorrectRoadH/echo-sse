17c9f183196f5d6177ba970c874484b7094634a6
CorrectRoadH <a778917369@gmail.com>
2023-07-11 11:52:12 +0800

    public the method

--- sse.go
+++ sse.go
@@ -7,22 +7,22 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-type sseClient struct {
+type SSEClient struct {
 	c echo.Context
 }
 
-func newSSEClint(c echo.Context) *sseClient {
+func NewSSEClint(c echo.Context) *SSEClient {
 	c.Response().Header().Set(echo.HeaderContentType, "text/event-stream")
 	c.Response().Header().Set(echo.HeaderCacheControl, "no-cache")
 	c.Response().Header().Set(echo.HeaderConnection, "keep-alive")
 
 	c.Response().WriteHeader(http.StatusOK)
-	return &sseClient{
+	return &SSEClient{
 		c: c,
 	}
 }
 
-func (c *sseClient) SendEvent(text string) error {
+func (c *SSEClient) SendEvent(text string) error {
 	w := c.c.Response().Writer
 	data := fmt.Sprintf("data: %s\n\n", string(text))
 	w.Write([]byte(data))
@@ -30,7 +30,7 @@ func (c *sseClient) SendEvent(text string) error {
 	return nil
 }
 
-func (c *sseClient) Close() error {
+func (c *SSEClient) Close() error {
 	w := c.c.Response().Writer
 	data := []byte("data: [DONE]\n\n")
 	w.Write(data)
